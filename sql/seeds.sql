230863a85e4588650a8a40dd8307432fe210eeb8
Amina Hayat <hayat.amina@hotmail.com>
2022-08-26 17:24:56 +0100

    seeds completed

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -19,3 +19,15 @@ VALUES
     ('Software Engineer', 75000, 3),
     ('Auditor', 37000, 4),
     ('Accountant', 24000, 4);
+
+INSERT INTO employee
+    (firstname, lastname, roleid, managerid)
+VALUES
+    ('Donald', 'Duck', 1, NULL),
+    ('Mickey', 'Mouse', 2, 1),
+    ('Spongebob', 'SquarePants', 3, NULL),
+    ('Patrick', 'Star', 4, 3),
+    ('Squidward', 'Tentacles', 5, NULL),
+    ('Homer', 'Simpson', 6, 5),
+    ('Ned', 'Flanders', 7, NULL),
+    ('Peppa', 'Pig', 8, 7);   
