6149da7b1dd46265c1479a1669ac7a036c7d4607
Amina Hayat <hayat.amina@hotmail.com>
2022-08-26 17:24:29 +0100

    seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -7,4 +7,15 @@ VALUES
     ('Legal'),
     ('IT'),
     ('Finance');
-    
+
+INSERT INTO role
+    (title, salary, departmentid)
+VALUES
+    ('Sales Consultant', 20000, 1),
+    ('Sales Representative', 30000, 1),
+    ('Barristor', 60000, 2),
+    ('Solicitor', 70000, 2),
+    ('Scrum Master', 85000, 3),
+    ('Software Engineer', 75000, 3),
+    ('Auditor', 37000, 4),
+    ('Accountant', 24000, 4);
