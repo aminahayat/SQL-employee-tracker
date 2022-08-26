c41adb76213b121f5d780165683b5a48443d0400
Amina Hayat <hayat.amina@hotmail.com>
2022-08-26 17:28:40 +0100

    added connections

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'password',
+      database: 'employeesdb'
+    },
+    console.log(`Connected to the employees database.`)
+  );
+
+  module.exports = db;  
